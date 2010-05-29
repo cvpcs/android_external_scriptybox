781feeeec82f6dee381cd438b4f614fc65d25cdf
cvpcs <root@cvpcs.org>
2010-05-29 10:55:31 -0500

    added adblock and halt commands removed shutdown command

--- Android.mk
+++ Android.mk
@@ -2,11 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+#########################################################
+# First we install the script
+#
+
 LOCAL_MODULE := scriptybox
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+
+#########################################################
+# Now we make our symlinks
+#
 
 SCRIPTYBOX_LINKS := $(shell cat $(LOCAL_PATH)/scriptybox.links)
 
@@ -28,3 +36,19 @@ ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
     $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
 
+#########################################################
+# Now we make copy our config files
+#
+
+etc_files := \
+	hosts.adblock \
+	hosts.local
+
+copy_to := $(addprefix $(TARGET_OUT)/etc/$(LOCAL_MODULE)/,$(etc_files))
+copy_from := $(addprefix $(LOCAL_PATH)/etc/,$(etc_files))
+
+$(copy_to) : PRIVATE_MODULE := system_etcdir
+$(copy_to) : $(TARGET_OUT)/etc/$(LOCAL_MODULE)/% : $(LOCAL_PATH)/etc/% | $(ACP)
+	$(transform-prebuilt-to-target)
+
+ALL_PREBUILT += $(copy_to)
